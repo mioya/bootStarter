6c7d7d70854f642fb30222b36ed70429de35db4e
mio <powalsld@naver.com>
2016-12-22 11:18:04 +0900

    gradle 프로젝트 커밋

---
+++ src/main/java/com/mio.controller/UserController.java
@@ -0,0 +1,16 @@
+package com.mio.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+public class UserController {
+
+    @RequestMapping("/")
+    public String index(Model model) {
+        model.addAttribute("name", "spring boot from");
+        return "index";
+    }
+}
---
+++ src/test/java/com/mio/ApplicationTests.java
@@ -0,0 +1,21 @@
+package com.mio;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class ApplicationTests {
+
+	@Test
+	public void contextLoads() {
+		assertTrue(true);
+	}
+
+}
