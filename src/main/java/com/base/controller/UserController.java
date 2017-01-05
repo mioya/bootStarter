53cefb7bd20b1bbd7857eed9a0899e8d30df886c
mio <powalsld@naver.com>
2017-01-05 18:49:21 +0900

    feat(dev):swagger 추가, 포트변경, runner 변경
    * swagger 디펜던시 추가 및 config 설정
    * 서버 포트 5005로 수정
    * mockito runner deprecated 되있어서 변경.

--- src/main/java/com/base/controller/UserController.java
+++ src/main/java/com/base/controller/UserController.java
@@ -1,15 +1,22 @@
 package com.base.controller;
 
+import lombok.extern.log4j.Log4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@Log4j
 public class UserController {
 
     @RequestMapping("/")
     public String index(Model model) {
         model.addAttribute("name", "sprinag boot from");
+        return "index";
+    }
+    @RequestMapping("/test")
+    public String test(Model model) {
+        model.addAttribute("test", "test");
         return "index";
     }
 }
--- src/test/java/com/base/ApplicationTests.java
+++ src/test/java/com/base/ApplicationTests.java
@@ -2,13 +2,8 @@ package com.base;
 
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.runners.MockitoJUnitRunner;
+import org.mockito.junit.MockitoJUnitRunner;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringRunner;
-
-import static org.hamcrest.Matchers.is;
-import static org.junit.Assert.assertThat;
-import static org.junit.Assert.assertTrue;
 
 @RunWith(MockitoJUnitRunner.class)
 @SpringBootTest
